c9deaed204739dcdc9aefd2600c3cacd23359db2
penge <ppenge@gmail.com>
2020-07-25 05:42:30 +0200

    Add Student model and context

---
+++ StudentsApi/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentsApi.Models
+{
+    [Table("students")]
+    public class Student
+    {
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("age")]
+        public int Age { get; set; }
+    }
+}
---
+++ StudentsApi/Models/StudentContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentsApi.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
